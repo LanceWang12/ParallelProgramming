a482120ed949a00a0e19e5594841508dbb82853d
lance <backfall52@gmail.com>
2022-10-30 08:36:30 +0800

    part1 init

---
+++ Hw2/part1/pi.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "simdxorshift128plus.h"
+using namespace std;
+
+// mutex
+pthread_mutex_t lock;
+
+// shared data between thread
+long long int total_number_in_circle = 0;
+
+double get_pi_seq(long long int number_of_tosses){
+    long long int number_in_circle = 0;
+    double x, y, distance_squared;
+    double min = -1, max = 1, numerator = max - min, denominator = RAND_MAX + 1.0; // diff: max - min, 
+    for (long long int toss = 0; toss < number_of_tosses; toss++){
+        x = numerator * rand() / denominator + min;
+        y = numerator * rand() / denominator + min;
+        distance_squared = x * x + y * y;
+        if ( distance_squared <= 1)
+            number_in_circle++;
+    }
+    long double pi_estimate = 4 * number_in_circle / (( long double ) number_of_tosses);
+    return pi_estimate;
+}
+
+typedef struct{
+    long long int number_of_tosses;
+    double min, numerator, denominator;
+} child_arg;
+
+void* child(void* args){
+    // get arg
+    child_arg *real_args = (child_arg*) args;
+    long long int number_of_tosses = real_args -> number_of_tosses;
+    double min = real_args -> min, numerator = real_args -> numerator, denominator = real_args -> denominator;
+
+    long long int number_in_circle = 0;
+    double x, y, distance_squared;
+    
+    for (long long int toss = 0; toss < number_of_tosses; toss++){
+        x = numerator * rand() / denominator + min;
+        y = numerator * rand() / denominator + min;
+        distance_squared = x * x + y * y;
+        if ( distance_squared <= 1)
+            number_in_circle++;
+    }
+
+    pthread_mutex_lock(&lock);
+    total_number_in_circle += number_in_circle;
+    pthread_mutex_unlock(&lock);
+
+    return (void*)0;
+}
+
+
+
+double get_pi_boost(long long int total_number_of_tosses, int thread_num = 1){
+    cout << thread_num << endl;
+    double min = -1, max = 1, numerator = max - min, denominator = RAND_MAX + 1.0; // diff: max - min
+    pthread_t threads[thread_num];
+
+    long long int  number_of_tosses = total_number_of_tosses / thread_num;
+    child_arg tmp = {number_of_tosses, min, numerator, denominator};
+    child_arg *args = &tmp;
+
+    for(int i = 0; i < thread_num; i++)
+        pthread_create(&threads[i], NULL, child, (void*) args);
+
+    for(int i = 0; i < thread_num; i++)
+        pthread_join(threads[i], NULL);
+
+    long double pi_estimate = 4 * total_number_in_circle / (( long double ) total_number_of_tosses);
+    return pi_estimate;
+}
+
+int main(int argc, char *argv[]){
+    assert(argc > 2);
+    // char array to long long int
+    int thread_num = atoi(argv[1]);
+    long long int tosses = atoll(argv[2]);
+    // cout << thread_num << ' ' << tosses << endl;
+    // long double pi = get_pi_seq(tosses);
+    pthread_mutex_init(&lock, NULL);
+    long double pi = get_pi_boost(tosses, thread_num);
+    // avx_xorshift128plus_key_t mykey;
+    // avx_xorshift128plus_init(324, 4444,&mykey);
+    // double pi = (double) avx_xorshift128plus(&mykey);
+
+    cout << pi << endl;
+    
+    return 0;
+}
